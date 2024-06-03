92076405d7a26c2c9287c83410ecaba1e198f583
Dari Charles <102997057+daricharles@users.noreply.github.com>
2024-06-03 18:18:04 +0000

    I changed the class name to from Main.java to GradeCalculator.java.

---
+++ src/main/java/GradeCalculator.java
@@ -0,0 +1,40 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Scanner;
+
+// import org.junit.jupiter.api.Test;
+
+public class GradeCalculator {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Enter your grade score here: ");
+
+    int grade = scan.nextInt();
+    char gradeScore;
+    
+    if (grade >= 90){
+      gradeScore = 'A';
+    }
+    else if (grade >= 80){
+      gradeScore = 'B';
+    }
+    else if (grade >= 70){
+      gradeScore = 'C';
+    }
+    else if (grade >= 60){
+      gradeScore = 'D';
+    }
+    else{
+      gradeScore = 'F';
+    }
+
+    System.out.println("The letter to your grade score is: " + gradeScore);
+
+    
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
